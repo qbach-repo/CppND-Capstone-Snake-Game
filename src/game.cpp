f1157641192d575920e97a973ca9437f582ff0da
qbach <58492405+qbach-repo@users.noreply.github.com>
2023-12-25 13:34:14 -0800

    Update game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -1,16 +1,29 @@
 #include "game.h"
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <future>
 #include "SDL.h"
+#include "snake.h"
 
 Game::Game(std::size_t grid_width, std::size_t grid_height)
     : snake(grid_width, grid_height),
       engine(dev()),
       random_w(0, static_cast<int>(grid_width - 1)),
       random_h(0, static_cast<int>(grid_height - 1)) {
+  _paused = false;
+  _wallUp = false;
   PlaceFood();
 }
 
-void Game::Run(Controller const &controller, Renderer &renderer,
+void TimerThread(Snake &snake) {
+  auto old_speed = snake.speed;
+  snake.speed *= 2.0;
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  snake.speed = old_speed + 0.02;
+}
+
+void Game::Run(Controller const &controller, Renderer *renderer,
                std::size_t target_frame_duration) {
   Uint32 title_timestamp = SDL_GetTicks();
   Uint32 frame_start;
@@ -23,9 +36,9 @@ void Game::Run(Controller const &controller, Renderer &renderer,
     frame_start = SDL_GetTicks();
 
     // Input, Update, Render - the main game loop.
-    controller.HandleInput(running, snake);
-    Update();
-    renderer.Render(snake, food);
+    controller.HandleInput(running, snake, *this);
+    Update(renderer);
+    renderer->Render(snake, food, &_wallUp);
 
     frame_end = SDL_GetTicks();
 
@@ -36,7 +49,7 @@ void Game::Run(Controller const &controller, Renderer &renderer,
 
     // After every second, update the window title.
     if (frame_end - title_timestamp >= 1000) {
-      renderer.UpdateWindowTitle(score, frame_count);
+      renderer->UpdateWindowTitle(score, frame_count);
       frame_count = 0;
       title_timestamp = frame_end;
     }
@@ -65,23 +78,51 @@ void Game::PlaceFood() {
   }
 }
 
-void Game::Update() {
+void Game::Update(Renderer *renderer) {
+  if (this->_paused == true) {
+    renderer->SetPauseScreen();
+    return;
+  }
   if (!snake.alive) return;
 
-  snake.Update();
+  snake.Update(&_wallUp);
 
   int new_x = static_cast<int>(snake.head_x);
   int new_y = static_cast<int>(snake.head_y);
 
+  std::random_device rd;  //Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::uniform_int_distribution<> dis(1, 10);
+
   // Check if there's food over here
   if (food.x == new_x && food.y == new_y) {
     score++;
+
     PlaceFood();
     // Grow snake and increase speed.
     snake.GrowBody();
-    snake.speed += 0.02;
+    if (dis(gen) <= 2) {
+      std::thread hyperTimer(TimerThread, snake);
+      hyperTimer.detach();
+    } else {
+      snake.speed += 0.02;
+    }
+
   }
 }
 
 int Game::GetScore() const { return score; }
 int Game::GetSize() const { return snake.size; }
+
+
+void Game::ToggleGamePause() {
+  this->_paused ? Resume() : Pause();
+}
+
+void Game::Pause() {
+  this->_paused = true;
+}
+
+void Game::Resume() {
+  this->_paused = false;
+}
